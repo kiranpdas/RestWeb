b228f9b409201a5be093a233b3d861139ce4b75a
Kiran Prasannadas <30925350+kiranpdas@users.noreply.github.com>
2019-02-25 19:27:19 -0500

    filtering alien based on id

--- src/main/java/RestAPITutorials/RestWeb/AlienRepository.java
+++ src/main/java/RestAPITutorials/RestWeb/AlienRepository.java
@@ -5,42 +5,43 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AlienRepository {
-	private List<Alien> aliens; ;
-	
+	private List<Alien> aliens;;
+
 	public AlienRepository() {
 		aliens = new ArrayList<>();
-		
-		Alien a1=new Alien();
+
+		Alien a1 = new Alien();
+		a1.setId(101);
 		a1.setName("Kiran");
 		a1.setPoints(99);
-		
+
 		aliens.add(a1);
-		
-		Alien a2=new Alien();
+
+		Alien a2 = new Alien();
+		a2.setId(102);
 		a2.setName("Anil");
 		a2.setPoints(98);
-		
+
 		aliens.add(a2);
-		
-		
+
 	}
-	
-	public List<Alien> getAliens(){
+
+	public List<Alien> getAliens() {
 		return this.aliens;
 	}
-	
-	public Alien getAlien(String name) {
-		
-		for(Alien alien : this.aliens) {
-			if(alien.getName().equals(name)) return alien;
+
+	public Alien getAlien(int alienId) {
+		for (Alien alien : this.aliens) {
+			if (alien.getId() == alienId)
+				return alien;
 		}
-		
+
 		return null;
 	}
 
 	public void create(Alien alien) {
 		this.aliens.add(alien);
-		
+
 	}
-	
+
 }
--- src/main/java/RestAPITutorials/RestWeb/AlienResource.java
+++ src/main/java/RestAPITutorials/RestWeb/AlienResource.java
@@ -1,12 +1,11 @@
 package RestAPITutorials.RestWeb;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.validation.constraints.Past;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -15,14 +14,21 @@ public class AlienResource {
 	AlienRepository alienRepo= new AlienRepository();
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public List<Alien> getAlien() {
+	public List<Alien> getAliens() {
 		return alienRepo.getAliens();
 	}
 	
+	@GET
+	@Path("alien/{id}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Alien getAlien(@PathParam("id") int alienId) {
+		return alienRepo.getAlien(alienId);
+	}
+	
 	@POST
-	@Past()
+	@Path("alien")
 	public Alien createAlien(Alien alien) {
-		
+		System.out.println(alien);
 		alienRepo.create(alien);
 		return alien;
 		
